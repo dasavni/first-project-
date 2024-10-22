2e22118db10aceffaf2c6e55c126baa07350935a
dasavni <avnidas2015@gmail.com>
2024-10-21 17:38:44 -0700

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,32 @@
+name="avni"
+age=9
+color="cream"
+likerain=True
+characteristics=[name,age,color,likerain]
+
+print(name)
+print(age)
+print(color)
+print(likerain)
+print(characteristics)
+
+age+=1
+print(age)
+
+age-=1
+print(age)
+
+idk="hello"+"world"
+
+divide=11572340%34934
+classtime=59.8
+classtime=((classtime/24)**6)-7
+print(idk, divide, classtime)
+
+practice=100.98
+if practice<100:
+    print ("rose")
+elif practice==100:
+    print ("lily")
+else :
+    print("daisy")
